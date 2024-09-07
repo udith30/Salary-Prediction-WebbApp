beb71c0822e78c10d1f54a1167ee69273ec44f89
udith30 <117038988+udith30@users.noreply.github.com>
2024-09-07 10:33:04 -0500

    Add files via upload

---
+++ predict_page.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+# Load the model and label encoders
+data = load_model()
+regressor_loaded = data["model"]
+le_country = data["le_country"]
+le_education = data["le_education"]
+
+def show_predict_page():
+    st.title("Software Developer Salary Prediction")
+
+    st.write("""### We need some information to predict the salary""")
+
+    # Country and Education options
+    countries = (
+        "United States",
+        "India",
+        "United Kingdom",
+        "Germany",
+        "Canada",
+        "Brazil",
+        "France",
+        "Spain",
+        "Australia",
+        "Netherlands",
+        "Poland",
+        "Italy",
+        "Russian Federation",
+        "Sweden",
+    )
+
+    education_levels = (
+        "Less than a Bachelors",
+        "Bachelor’s degree",
+        "Master’s degree",
+        "Post grad",
+    )
+
+    # User inputs
+    country = st.selectbox("Country", countries)
+    education = st.selectbox("Education Level", education_levels)
+    experience = st.slider("Years of Experience", 0, 50, 3)
+
+    ok = st.button("Calculate Salary")
+    if ok:
+        # Convert inputs to array
+        X = np.array([[country, education, experience]])
+
+        # Apply label encoding
+        X[:, 0] = le_country.transform(X[:, 0])
+        X[:, 1] = le_education.transform(X[:, 1])
+        X = X.astype(float)
+
+        # Predict salary using the loaded model
+        salary = regressor_loaded.predict(X)
+        
+        # Display result
+        st.subheader(f"The estimated salary is ${salary[0]:.2f}")
+
+# Run the Streamlit app
+show_predict_page()
